7964b240b20b2aa0f8b4c845a72d9d4aebdaf77d
Aslm <khelifiaslem@yahoo.fr>
2021-05-22 04:17:23 +0100

    Updates

---
+++ precache-manifest.d0429b3cc77318f55dc21f400dcfd9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11daec7dd3ae04ded3b1",
+    "url": "/propcheckpt/static/css/main.38e316d8.chunk.css"
+  },
+  {
+    "revision": "11daec7dd3ae04ded3b1",
+    "url": "/propcheckpt/static/js/main.48dedf67.chunk.js"
+  },
+  {
+    "revision": "40bdf13fb6421fc2eb52",
+    "url": "/propcheckpt/static/js/runtime~main.a1b19f09.js"
+  },
+  {
+    "revision": "b2709792acf12e570620",
+    "url": "/propcheckpt/static/js/2.e651c8a1.chunk.js"
+  },
+  {
+    "revision": "ba9eda7c4f2c63aa715a",
+    "url": "/propcheckpt/static/js/3.7d56d9a2.chunk.js"
+  },
+  {
+    "revision": "2cb6f9be6bc7b068f409411589126be0",
+    "url": "/propcheckpt/index.html"
+  }
+];
